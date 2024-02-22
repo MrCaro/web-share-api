c15bc33825a11a7a5542034b80a80257154a707a
MrCaro <36524539+MrCaro@users.noreply.github.com>
2024-02-21 22:17:35 -0500

    feat: adding copy link btn and refactor folder structure

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-let params = new URLSearchParams(document.location.search)
+const url = window.location.href
+const params = new URLSearchParams(window.location.search)
 
-let score = params.get('score')
-
-let title = 'Find out your score today!'
-
+const score = params.get('score')
+const title = 'Find out your score today!'
 const text = `I scored ${score ?? '(missing score value)'}`
 
 const shareData = {
@@ -11,14 +10,31 @@ const shareData = {
   text: text,
   url: "https://developer.mozilla.org",
 };
-const btn = document.querySelector("button");
-const resultPara = document.querySelector(".result");
+
+const btn = document.querySelector("button")
+const btnURL = document.getElementById("btnCopyURL")
+const resultPara = document.querySelector(".result")
+
 // Share must be triggered by “user activation”
 btn.addEventListener("click", async () => {
-  try {
-    await navigator.share(shareData);
-    resultPara.textContent = "Score shared successfully";
-  } catch (err) {
-    resultPara.textContent = `Error: ${err}`;
+  if (navigator.share) {
+    try {
+      await navigator.share(shareData);
+      resultPara.textContent = "Score shared successfully"
+    } catch (err) {
+      resultPara.textContent = `Error: ${err}`
+    }
+  } else {
+    // btnURL.style.display = 'none'
+    alert('showing this popup')
   }
 });
+
+btnURL.addEventListener("click", async () => {
+  try {
+    await navigator.clipboard.writeText(url)
+    alert(`Link ${url} copied to clipboard!`);
+  } catch(err) {
+    console.error(err.message)
+  }
+})
